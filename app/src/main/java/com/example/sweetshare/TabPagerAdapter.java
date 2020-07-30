f9953d742e2f88f553c09d98105c7a832a9531db
bmarcu13 <bogdanmarcu2003@gmail.com>
2020-07-30 14:50:10 +0300

    Added initial main navigation.

---
+++ app/src/main/java/com/example/sweetshare/TabPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.sweetshare;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabPagerAdapter extends FragmentStateAdapter {
+    public TabPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new ProfileTab();
+            case 1:
+                return new ExploreTab();
+            case 2:
+                return new MessagesTab();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
